0ac06dc23ce37e5a0fd7b4c20408533545ceec63
e7e26e5 <abc@xyz.com>
2023-06-20 17:03:16 +0800

    refactor

--- stl/unordmultiset2.cpp
+++ stl/unordmultiset2.cpp
@@ -19,7 +19,7 @@ int main() {
     coll.insert({1, 3, 5, 7, 11, 13, 17, 19, 23, 27, 1});
 
     // print all elements
-    for (auto elem: coll) {
+    for (const auto &elem: coll) {
         std::cout << elem << ' ';
     }
     std::cout << std::endl;
@@ -28,7 +28,7 @@ int main() {
     coll.insert(25);
 
     // print all elements again
-    for (auto elem: coll) {
+    for (const auto &elem: coll) {
         std::cout << elem << ' ';
     }
     std::cout << std::endl;
